170076a52796c630e33b32eb4af12952781d4aac
John Heaphey <61230540+towerdef-game@users.noreply.github.com>
2021-04-27 15:48:10 +0100

    Update tail.cs

--- Assets/tail.cs
+++ Assets/tail.cs
@@ -5,6 +5,21 @@ using UnityEngine;
 public class tail : MonoBehaviour
 
 {
+    public float swingrate;
+    public Transform dogstail;
+    public float tailRadius;
+    // Start is called before the first frame update
+    void Start()
+    {
 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dogstail.localRotation = Quaternion.RotateTowards(dogstail.localRotation, Quaternion.Euler(0, tailRadius, 0), swingrate);
+        if (dogstail.localRotation == Quaternion.Euler(0, tailRadius, 0)) tailRadius = -tailRadius;
+    }
 }
 
